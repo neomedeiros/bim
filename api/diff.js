fc0ceca752eab91cc92e56cdf8bc521ac6108ed5
Leandro Medeiros <medeirosleandro1982@gmail.com>
2018-04-11 01:30:43 -0300

    handle reject on api methods

--- api/diff.js
+++ api/diff.js
@@ -8,24 +8,37 @@ var jsonParser = bodyParser.json();
 var diffService = new DiffService();
 
 router.post('/left', jsonParser, function (req, res) {    
-    diffService.storeValue(req.body.code, 'left', req.body.value).then((diffEntry)=>{
+    diffService.storeValue(req.body.code, 'left', req.body.value)
+    .then((diffEntry)=>{
         res.status(200).send(diffEntry);
-    });            
+    })
+    .catch((err) => {
+        res.status(400).send('uh-oh, something wrong.');
+    })
+    ;            
 });
 
 router.post('/right', jsonParser, function (req, res) {
-    diffService.storeValue(req.body.code, 'right', req.body.value).then((diffEntry) => {
+    diffService.storeValue(req.body.code, 'right', req.body.value)
+    .then((diffEntry) => {
         res.status(200).send(diffEntry);
+    })
+    .catch((err) => {
+        res.status(400).send('uh-oh, something wrong.');
     });    
 });
 
 router.get('/compare/:code', jsonParser, function (req, res) {
     
-    diffService.compare(req.params.code).then(
+    diffService.compare(req.params.code)
+    .then(
         (result) => {
             res.status(200).send(result);
         }
-    );    
+    )
+    .catch((err) => {
+        res.status(400).send('unable to compare de values.');
+    });    
 });
 
 module.exports = router;
